21c28c209f981709ee728791419fe8e51a015915
Pere Casas <pcasas.pere@gmail.com>
2026-02-11 23:43:02 +0100

    chore: remove legacy python code directory

--- legacy/FieldWatcher/__init__.py
+++
@@ -1,8 +0,0 @@
-from .Config import ConfigManager
-from .Backend import ApiManager
-from .Sniffer import SnifferManager
-from .Utils import verbose, verbose_error
-from .DB import Database
-from .Objects import Connection, Asset
-
-__all__ = ['ConfigManager', 'ApiManager', 'SnifferManager', 'verbose', 'verbose_error', 'Database', 'Connection', 'Asset']
